4a82ea4784d6e14636cafb8a2b91d1ed9d229037
Shivanshi Sharma <shivanshisharma024@gmail.com>
2024-09-01 18:57:44 +0530

    program on insertion sort

---
+++ DS/sorting/insertion_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void insSort(int arr[], int n) {
+    int i, j, k;
+    for (i = 1; i < n; i++) {
+        k = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > k) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = k;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
